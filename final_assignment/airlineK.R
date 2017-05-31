4d9234315de9f8a0dc723bc0423d62b73fe752b7
kjod <kieranjamesodriscoll@gmail.com>
2017-05-31 21:32:37 +0200

    Added csv for data

--- final_assignment/airlineK.R
+++
@@ -1,7 +0,0 @@
-airline=read.csv("airline-safety.csv", header=TRUE)
-
-### Analysis of data
-boxplot(airline$incidents_85_99, airline$incidents_00_14)
-boxplot(airline$fatal_accidents_85_99, airline$fatal_accidents_00_14)
-boxplot(airline$fatalities_85_99, airline$fatalities_00_14)
-
